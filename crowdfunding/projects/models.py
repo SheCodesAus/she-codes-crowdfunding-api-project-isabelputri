2ff64b28d0c9b59ccc7ab446d8d56d3af69b10c9
isabelputri <isabelputri.ip@gmail.com>
2023-01-14 12:29:26 +0800

    setup

--- crowdfunding/projects/models.py
+++ crowdfunding/projects/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    goal = models.IntegerField()
+    image = models.URLField()
+    is_open = models.BooleanField() #checking whether the project is active or not
+    date_created = models.DateTimeField(auto_now_add=True)
+    owner = models.CharField(max_length=200)
