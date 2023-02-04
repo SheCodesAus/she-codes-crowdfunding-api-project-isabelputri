e40b7d6fecabd1f52ebb50b01ad39d8aff99ccbe
isabelputri <isabelputri.ip@gmail.com>
2023-02-04 21:05:38 +0800

    Fixed password

--- crowdfunding/users/serializers.py
+++ crowdfunding/users/serializers.py
@@ -9,4 +9,19 @@ class CustomUserSerializer(serializers.Serializer):
     def create(self, validated_data):
         return CustomUser.objects.create(**validated_data)
 
+class CustomUserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = CustomUser
+        fields = ['email', 'username', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
+    
+    def create(self, validated_data):
+        user = CustomUser(
+            email=validated_data['email'],
+            username=validated_data['username']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
 
