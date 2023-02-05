7eaabef7b71c2d947c8c32e9792589876b4554ef
isabelputri <isabelputri.ip@gmail.com>
2023-02-05 23:17:09 +0800

    not working - update + delete user

--- crowdfunding/users/serializers.py
+++ crowdfunding/users/serializers.py
@@ -1,25 +1,22 @@
+from rest_framework.validators import UniqueValidator
 from rest_framework import serializers
 from .models import CustomUser
-
+from django.contrib.auth.password_validation import validate_password
 class CustomUserSerializer(serializers.Serializer):
     id = serializers.ReadOnlyField()
-    username = serializers.CharField(max_length=150)
-    email = serializers.EmailField()
-
-    def create(self, validated_data):
-        return CustomUser.objects.create(**validated_data)
-
-class CustomUserSerializer(serializers.ModelSerializer):
-
+    username = serializers.CharField(max_length=200)
+    email = serializers.EmailField(
+        required=True,
+        validators=[UniqueValidator(queryset=CustomUser.objects.all())]
+        )
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
     class Meta:
         model = CustomUser
-        fields = ['email', 'username', 'password']
-        extra_kwargs = {'password': {'write_only': True}}
-    
+        fields = ('id', 'username', 'password', 'email')
     def create(self, validated_data):
-        user = CustomUser(
+        user = CustomUser.objects.create(
+            username=validated_data['username'],
             email=validated_data['email'],
-            username=validated_data['username']
         )
         user.set_password(validated_data['password'])
         user.save()
@@ -33,3 +30,39 @@ class ChangePasswordSerializer(serializers.Serializer):
     """
     old_password = serializers.CharField(required=True)
     new_password = serializers.CharField(required=True)
+
+class CustomUserSerializerUpdate(CustomUserSerializer):
+    def update(self, instance, validated_data):
+        instance.username = validated_data.get('username',instance.username)
+        instance.email = validated_data.get('email', instance.email)
+        if password := validated_data.get('password'):
+            instance.set_password(password)
+        instance.save()
+        return instance
+
+##### DRAFT 1 ####
+# class CustomUserSerializer(serializers.Serializer):
+#     id = serializers.ReadOnlyField()
+#     username = serializers.CharField(max_length=150)
+#     email = serializers.EmailField()
+
+#     def create(self, validated_data):
+#         return CustomUser.objects.create(**validated_data)
+
+# class CustomUserSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = CustomUser
+#         fields = ['id', 'first_name', 'last_name', 'username', 'email', 'password']
+#         read_only_fields = ['id']
+#         extra_kwargs = {"password":{"write_only":True}}
+
+#     def create(self, validated_data):
+#         user = CustomUser.objects.create(
+#             first_name = validated_data['first_name'],
+#             last_name = validated_data['last_name'],
+#             username = validated_data['username'],
+#             email = validated_data['email']
+#         )
+#         user.set_password(validated_data['password'])
+#         user.save()
+#         return user
--- crowdfunding/users/views.py
+++ crowdfunding/users/views.py
@@ -1,11 +1,12 @@
 from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status, generics
+from rest_framework import status, generics, permissions
 
 from .models import CustomUser
-from .serializers import CustomUserSerializer, ChangePasswordSerializer
-from rest_framework.permissions import IsAuthenticated   
+from .serializers import CustomUserSerializer, ChangePasswordSerializer, CustomUserSerializerUpdate
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from .permissions import IsOwnerOrReadOnly
 
 class CustomUserList(APIView):
 
@@ -22,7 +23,11 @@ class CustomUserList(APIView):
         return Response(serializer.errors)
 
 class CustomUserDetail(APIView):
-    
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly,
+        IsOwnerOrReadOnly
+    ]
+
     def get_object(self, pk):
         try:
             return CustomUser.objects.get(pk=pk)
@@ -33,14 +38,32 @@ class CustomUserDetail(APIView):
         user = self.get_object(pk)
         serializer = CustomUserSerializer(user)
         return Response(serializer.data)
-        
+
+    def put(self, request, pk):
+        user = self.get_object(pk)
+        data = request.data
+        serializer = CustomUserSerializerUpdate(
+			instance=user,
+			data=data,
+			partial=True
+		)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.error,status=status.HTTP_400_BAD_REQUEST)
+	
+    def delete(self, request,pk):
+        user = self.get_object(pk)
+        user.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
 class ChangePasswordView(generics.UpdateAPIView):
     """
     An endpoint for changing password.
     """
     serializer_class = ChangePasswordSerializer
     model = CustomUser
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticatedOrReadOnly,)
 
     def get_object(self, queryset=None):
         obj = self.request.user
@@ -67,3 +90,19 @@ class ChangePasswordView(generics.UpdateAPIView):
             return Response(response)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class CustomUserDetail(APIView):
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly,
+        IsOwnerOrReadOnly
+    ]
+    def get_object(self, pk):
+        try:
+            return CustomUser.objects.get(pk=pk)
+        except CustomUser.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        user = self.get_object(pk)
+        serializer = CustomUserSerializer(user)
+        return Response(serializer.data)
